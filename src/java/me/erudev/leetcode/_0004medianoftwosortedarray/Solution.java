eb3250c83562b0d06f959b5083108a6d8cf16836
zhaopengfei <zhaopengfei@yishanol.com>
2021-06-15 14:58:20 +0800

    :sparkles:median of two sorted array

---
+++ src/java/me/erudev/leetcode/_0004medianoftwosortedarray/Solution.java
@@ -0,0 +1,65 @@
+package me.erudev.leetcode._0004medianoftwosortedarray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 题目描述
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * 示例 3：
+ * <p>
+ * 输入：nums1 = [0,0], nums2 = [0,0]
+ * 输出：0.00000
+ * 示例 4：
+ * <p>
+ * 输入：nums1 = [], nums2 = [1]
+ * 输出：1.00000
+ * 示例 5：
+ * <p>
+ * 输入：nums1 = [2], nums2 = []
+ * 输出：2.00000
+ *
+ * @author pengfei.zhao
+ * @date 2021/6/15 14:19
+ */
+public class Solution {
+
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            /**
+             * 循环中该怎么写，什么时候 A 数组后移，什么时候 B 数组后移。用 aStart 和 bStart 分别表示当前指向 A 数组和 B 数组的位置。
+             * 如果 aStart 还没有到最后并且此时 A 位置的数字小于 B 位置的数组，那么就可以后移了。也就是aStart＜m&&A[aStart]< B[bStart]。
+             *
+             */
+            if (aStart < m && (bStart >= n || A[aStart] < B[bStart])) {
+                right = A[aStart++];
+            } else {
+                right = B[bStart++];
+            }
+        }
+        if ((len & 1) == 0)
+            return (left + right) / 2.0;
+        else
+            return right;
+    }
+
+
+}
