915430ae384cdfb169e9cb3141869878aec7d658
zhaopengfei <zhaopengfei@yishanol.com>
2021-06-18 15:17:42 +0800

    :card_index:0005 longest palindromic substring

---
+++ src/java/me/erudev/leetcode/_0005longestpalindromicsubstring/Solution.java
@@ -0,0 +1,62 @@
+package me.erudev.leetcode._0005longestpalindromicsubstring;
+
+/**
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * <p>
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * <p>
+ * 示例 3：
+ * 输入：s = "a"
+ * 输出："a"
+ * <p>
+ * 示例 4：
+ * 输入：s = "ac"
+ * 输出："a"
+ *
+ * @author pengfei.zhao
+ * @date 2021/6/15 15:01
+ */
+public class Solution {
+    public static String longestPalindrome(String s) {
+        if (s.equals(""))
+            return "";
+        String origin = s;
+        String reverse = new StringBuffer(s).reverse().toString(); //字符串倒置
+        int length = s.length();
+        int[][] arr = new int[length][length];
+        int maxLen = 0;
+        int maxEnd = 0;
+        for (int i = 0; i < length; i++)
+            for (int j = 0; j < length; j++) {
+                if (origin.charAt(i) == reverse.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = arr[i - 1][j - 1] + 1;
+                    }
+                }
+                if (arr[i][j] > maxLen) {
+                    maxLen = arr[i][j];
+                    maxEnd = i; //以 i 位置结尾的字符
+                }
+
+            }
+        return s.substring(maxEnd - maxLen + 1, maxEnd + 1);
+    }
+
+    public static void main(String[] args) {
+        String s = "bacab";
+        String res = longestPalindrome(s);
+        System.out.println(res);
+    }
+}
+
